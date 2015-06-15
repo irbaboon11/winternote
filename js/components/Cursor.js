e8f5f486787b02c700dd45a49ffe21a31e423151
irbaboon11 <vkapffks@gmail.com>
2015-06-16 00:46:24 +0900

    Revert invalid Code

--- js/components/Cursor.js
+++ js/components/Cursor.js
@@ -1,49 +1,39 @@
-/*jshint node: true*/
+/*jshint browser: true, quotmark:false*/
 'use strict';
 
 var React = require('react/addons'),
     ViewStore = require('../stores/ViewStore'),
-    NoteConstants = require('../constants/NoteConstants'),
-    _ = require('lodash');
+    NoteConstants = require('../constants/NoteConstants');
 
 module.exports = React.createClass({
-  timeoutId: null,
+  displayName: 'Cursor',
+
   getInitialState: function() {
     return this._getState();
   },
 
   componentDidMount: function() {
     ViewStore.addChangeListener(this._onChange, NoteConstants.EVENT.RENDER);
-    this.setTimer();
   },
 
   componentWillUnmount: function() {
     ViewStore.removeChangeListener(this._onChange, NoteConstants.EVENT.RENDER);
-    clearTimeout(this.timeoutId);
-  },
-
-  setTimer: function() {
-    this.timeoutId = setTimeout(function() {
-      var current = this.getDOMNode().style.display;
-      if (current === 'none')
-        this.getDOMNode().style.display = 'block';
-      else
-        this.getDOMNode().style.display = 'none';
-      this.setTimer();
-    }.bind(this), 500);
   },
 
   render: function () {
-    var point = this.state.cursor;
     // TODO refactor editingArea rect
     var editingArea = document.getElementsByClassName('note-editing-area')[0];
     var rect = editingArea && editingArea.getBoundingClientRect();
+    var classes = React.addons.classSet({
+      'note-cursor': true,
+      'note-cursor-composition': this.state.isComposition
+    });
 
     var style;
     if (this.state.cursor) {
       style = {
         display: 'block',
-        left: this.state.cursor.left - rect.left - 2,
+        left: this.state.cursor.left - rect.left - 17,
         top: this.state.cursor.top - rect.top
       };
     } else {
@@ -53,7 +43,7 @@ module.exports = React.createClass({
     }
 
     // TODO addClass note-cursor-blink after 500ms for blink cursor
-    return <div className='note-cursor' style={style}></div>;
+    return <div className={classes} style={style}></div>;
   },
 
   _onChange: function () {
@@ -64,7 +54,8 @@ module.exports = React.createClass({
     var data = ViewStore.getData();
 
     return {
-      cursor: data.cursor
+      cursor: data.cursor,
+      isComposition: data.isComposition
     };
   }
 });
